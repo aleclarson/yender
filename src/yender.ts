8d760f6daa37299554e2ac5ad759d8f8aa66aa0a
aleclarson <alec.stanford.larson@gmail.com>
2018-09-26 10:40:51 -0400

    feat: add @yender decorator

--- src/yender.ts
+++ src/yender.ts
@@ -1,7 +1,10 @@
-import React from 'react'
+import { cloneElement, Component, ComponentClass } from 'react'
 import { immutagen } from './immutagen'
 
 export * from './render-props'
+
+/** The constructor of generator functions */
+const GeneratorFunction = function*(): any {}.constructor
 
 /** Extract the argument types of a function */
 type In<T> = T extends (...args: infer U) => any ? U : []
@@ -24,17 +27,42 @@ export interface Yender<Render> {
 /** Convert a generator to a stateless component */
 export function yender<
   Yield extends Element,
-  Render extends (props?: object, context?: any) => Iterator<Yield>
->(render: Render): Yender<Render> {
+  Render extends (props?: object) => Iterator<Yield>
+>(render: Render): Yender<Render>
+
+/** Decorate a component class */
+export function yender(componentType: ComponentClass): void
+
+/** @internal */
+export function yender<
+  Yield extends Element,
+  Render extends (props?: object) => Iterator<Yield>
+>(arg: Render | ComponentClass): Yender<Render> | void {
+  if (arg.prototype instanceof Component) {
+    return decorate(arg as ComponentClass)
+  }
+  const render = arg as Render
   const gen: (...args: In<Render>) => Yenderator<Yield> = immutagen(render)
   const compose = (ctx: Yenderator<Yield>): Element =>
     ctx.next
-      ? React.cloneElement(ctx.value, undefined, (props: object) =>
+      ? cloneElement(ctx.value, undefined, (props: object) =>
           compose(ctx.next!(props))
         )
       : ctx.value
 
-  const component = (...args: In<Render>) => compose(gen(...args))
-  component.displayName = 'YenderComponent'
-  return component
+  const componentType = (...args: In<Render>) => compose(gen(...args))
+  componentType.displayName = 'YenderComponent'
+  return componentType
+}
+
+/** Wrap the `render` method of a component class */
+function decorate(componentType: ComponentClass): void {
+  const { render } = componentType.prototype
+  if (render instanceof GeneratorFunction) {
+    Object.defineProperty(componentType.prototype, 'render', {
+      value: yender(render),
+    })
+  } else {
+    throw Error('Cannot use @yender when `render` is not a generator function')
+  }
 }
